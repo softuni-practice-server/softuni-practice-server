20016178e842208c4fd83bbfd88142c2b9b4b855
Viktor Kostadinov <viktor.pts@abv.bg>
2020-11-16 17:42:38 +0200

    Added missing OPTIONS method

--- crud.js
+++ crud.js
@@ -78,6 +78,18 @@ const controllers = {
                 responseData = responseData[token];
             }
         }
+    },
+    options: (req, res, tokens) => {
+        const headers = {};
+
+        headers['Access-Control-Allow-Origin'] = '*';
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS';
+        headers['Access-Control-Allow-Credentials'] = false;
+        headers['Access-Control-Max-Age'] = '86400'; // 24 hours
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
+
+        res.writeHead(200, headers);
+        res.end();
     }
 };
 
