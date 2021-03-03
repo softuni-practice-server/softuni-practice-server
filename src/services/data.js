2fbe9ab52d597628916c0a67433a2b69f17b1bee
Viktor Kostadinov <viktor.pts@abv.bg>
2021-03-03 20:08:41 +0200

    Greatly improved WHERE query

--- src/services/data.js
+++ src/services/data.js
@@ -23,6 +23,50 @@ function validateRequest(context, tokens, query) {
     }
 }
 
+function parseWhere(query) {
+    const operators = {
+        '<=': (prop, value) => record => record[prop] <= JSON.parse(value),
+        '<': (prop, value) => record => record[prop] < JSON.parse(value),
+        '>=': (prop, value) => record => record[prop] >= JSON.parse(value),
+        '>': (prop, value) => record => record[prop] > JSON.parse(value),
+        '=': (prop, value) => record => record[prop] == JSON.parse(value),
+        ' like ': (prop, value) => record => record[prop].includes(JSON.parse(value)),
+        ' in ': (prop, value) => record => JSON.parse(`[${/\((.+?)\)/.exec(value)[1]}]`).includes(record[prop]),
+    };
+    const pattern = new RegExp(`^(.+?)(${Object.keys(operators).join('|')})(.+?)$`, 'i');
+
+    try {
+        let clauses = [query.trim()];
+        let check = (a, b) => b;
+        let acc = true;
+        if (query.match(/ and /gi)) {
+            // inclusive
+            clauses = query.split(/ and /gi);
+            check = (a, b) => a && b;
+            acc = true;
+        } else if (query.match(/ or /gi)) {
+            // optional
+            clauses = query.split(/ or /gi);
+            check = (a, b) => a || b;
+            acc = false;
+        }
+        clauses = clauses.map(createChecker);
+
+        return (record) => clauses
+            .map(c => c(record))
+            .reduce(check, acc);
+    } catch (err) {
+        throw new Error('Could not parse WHERE clause, check your syntax.');
+    }
+
+    function createChecker(clause) {
+        let [match, prop, operator, value] = pattern.exec(clause);
+        [prop, value] = [prop.trim(), value.trim()];
+
+        return operators[operator.toLowerCase()](prop, value);
+    }
+}
+
 
 function get(context, tokens, query, body) {
     validateRequest(context, tokens, query);
@@ -31,8 +75,7 @@ function get(context, tokens, query, body) {
 
     try {
         if (query.where) {
-            const [prop, value] = query.where.split('=');
-            responseData = context.storage.query(context.params.collection, { [prop]: JSON.parse(value) });
+            responseData = context.storage.get(context.params.collection).filter(parseWhere(query.where));
         } else if (context.params.collection) {
             responseData = context.storage.get(context.params.collection, tokens[0]);
         } else {
@@ -115,7 +158,11 @@ function get(context, tokens, query, body) {
 
     } catch (err) {
         console.error(err);
-        throw new NotFoundError();
+        if (err.message.includes('does not exist')) {
+            throw new NotFoundError();
+        } else {
+            throw new RequestError(err.message);
+        }
     }
 
     return responseData;
