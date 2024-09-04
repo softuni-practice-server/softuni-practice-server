a6a935270564e5493f788dfda4e03aac46095ed6
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-09-04 17:52:06 +0300

    Export server object

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const plugins = [
 const server = http.createServer(createHandler(plugins, services));
 
 const port = 3030;
+
 server.listen(port);
+
 console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
 console.log(`Admin panel located at http://localhost:${port}/admin`);
+
+module.exports = server;
